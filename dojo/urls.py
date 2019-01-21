74dd00c0d6e1be244ee165b9115a592e0baf3770
changhoi0522 <changhoi0522@naver.com>
2019-01-21 23:30:09 +0900

    함수 기반 뷰의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import re_path, path, include
+from . import views_cbv
 from . import views
 
 urlpatterns = [
@@ -10,5 +11,9 @@ urlpatterns = [
     re_path(r'^list3/$', views.post_list3),
     re_path(r'^excel/$', views.excel_download),
 
+    re_path(r'^cbv/list1/$', views_cbv.post_list1),
+    re_path(r'^cbv/list2/$', views_cbv.post_list2),
+    re_path(r'^cbv/list3/$', views_cbv.post_list3),
+    re_path(r'^cbv/excel/$', views_cbv.excel_download),
 
 ]
