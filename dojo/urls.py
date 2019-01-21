28200eaed9b634b2f31b30f048441a383eda60f3
changhoi0522 <changhoi0522@naver.com>
2019-01-21 16:28:46 +0900

    dojo.views.mysum에서 다수 숫자를 받도록 개선

--- dojo/urls.py
+++ dojo/urls.py
@@ -3,7 +3,5 @@ from django.urls import re_path, path, include
 from . import views
 
 urlpatterns = [
-    re_path(r'^sum/(?P<x>\d+)$', views.mysum),
-    re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)$', views.mysum),
-    re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)$', views.mysum),
+    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -2,6 +2,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def mysum(request, x=0, y=0, z=0):
-    return HttpResponse(int(x) + int(y) + int(z))
+def mysum(request, numbers):
+    result = sum(map(lambda s: int(s or 0), numbers.rstrip('/').split('/')))
+    return HttpResponse(result)
+    # numbers = '1/2/3/4/5/6 ...'
     #request: HttpRequest
