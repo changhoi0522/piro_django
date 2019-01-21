8b37727cea3abeff2ab694ab6b9a895d26e2df19
changhoi0522 <changhoi0522@naver.com>
2019-01-21 22:32:54 +0900

    함수 기바 뷰의 4가지 패턴

--- dojo/views.py
+++ dojo/views.py
@@ -1,6 +1,8 @@
 # dojo/views.py
-from django.http import HttpResponse
+import os
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.conf import settings
 
 def mysum(request, numbers):
     result = sum(map(lambda s: int(s or 0), numbers.rstrip('/').split('/')))
@@ -10,3 +12,33 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
+
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분의 파이썬&장고 페이스메이커가 되어드리겠습니다. </p>
+    '''.format(name = name))
+
+def post_list2(request):
+    name = '공유'
+    response = render(request, 'dojo/post_list.html', {'name': name})
+    return response
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celary', 'Azure']
+    }, json_dumps_params = {'ensure_ascii': False})
+
+
+def excel_download(request):
+    filepath = os.path.join(settings.BASE_DIR, 'test.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
