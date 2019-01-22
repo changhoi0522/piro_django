ff29ad22669deb5fc13056e2cd9ea927ef6d28cf
changhoi0522 <changhoi0522@naver.com>
2019-01-22 15:45:33 +0900

    blog.admin.PostAdmin에list_display 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'content']
+    list_display = ['id', 'name','content_size', 'content']
+
+    def content_size(self, post):
+        return '{}'.format(len(post.content))
+    content_size.short_description = '글자수'
 
 admin.site.register(Post, PostAdmin) # 참고: 같은 모델 중복 등록은 불가
