e3656cbc1ea6d37afb872ba769d20aa01f33b306
changhoi0522 <changhoi0522@naver.com>
2019-01-25 01:55:28 +0900

    ep12 is done

--- piro_django/settings.py
+++ piro_django/settings.py
@@ -63,7 +63,9 @@ ROOT_URLCONF = 'piro_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'piro_django', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
