d406776dbd1c2198e08b48eab582fa67edf0cdbe
changhoi0522 <changhoi0522@naver.com>
2019-01-29 23:38:39 +0900

    dojo.views.post_new 뷰 구현

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,6 +4,8 @@ from . import views_cbv
 from . import views
 
 urlpatterns = [
+    re_path(r'^new/$', views.post_new),
+
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     re_path(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
     re_path(r'^list1/$', views.post_list1),
