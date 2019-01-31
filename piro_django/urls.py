c0785f39e1228d60459697721d512ff271ca87f7
changhoi0522 <changhoi0522@naver.com>
2019-01-31 15:10:49 +0900

    media 파일 서빙 지원

--- piro_django/urls.py
+++ piro_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import re_path, include
@@ -25,7 +26,7 @@ urlpatterns = [
     re_path(r'^accounts/', include('accounts.urls')),
     re_path(r'^shop/', include('shop.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import  debug_toolbar
