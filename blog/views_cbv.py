8940054bcd1255554a4e392ea9c9d1e2169a87ce
changhoi0522 <changhoi0522@naver.com>
2019-02-01 21:28:51 +0900

    CBV: DeleteView 구현

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .models import Post
 from django import forms
 
@@ -10,3 +10,5 @@ post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 post_new = CreateView.as_view(model=Post, fields='__all__')
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
+
+post_delete = DeleteView.as_view(model=Post, success_url="/blog/")
