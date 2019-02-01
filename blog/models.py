df2382a9c9b9010837e580feb0a9e1b9d9d52ca5
changhoi0522 <changhoi0522@naver.com>
2019-02-01 14:09:45 +0900

    imagekit을 활용해서 이미지가 업로드 될 때, 썸네일 처리된 이미지만 저장

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ import re
 from django.conf import settings
 from django.forms import ValidationError
 from django.db import models
-from imagekit.models import ImageSpecField
+from imagekit.models import ProcessedImageField
 from imagekit.processors import Thumbnail
 
 # Create your models here.
@@ -24,11 +24,10 @@ class Post(models.Model):
     name = models.CharField(max_length=100, verbose_name='제목', help_text='제목을 입력해주세요. 최대 100자')
     content = models.TextField(verbose_name='내용')
 
-    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
-    photo_thumbnail = ImageSpecField(source='photo',
-                                     processors=[Thumbnail(300, 300)],
-                                     format='JPEG',
-                                     options={'quality': 60})
+    photo = ProcessedImageField(blank=True, upload_to='blog/post/%Y/%m/%d',
+                                processors=[Thumbnail(300, 300)],
+                                format='JPEG',
+                                options={'quality': 60})
 
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50,
