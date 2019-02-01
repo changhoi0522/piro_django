b42fb0e038c9788f69d2504273e0be062bec14a8
changhoi0522 <changhoi0522@naver.com>
2019-02-01 19:45:52 +0900

    'CBV 샘플 - STEP 4. CBV 구현

--- dojo/views.py
+++ dojo/views.py
@@ -3,7 +3,7 @@ import os
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
-
+from django.views.generic import DetailView
 from dojo.forms import PostForm
 from dojo.models import Post
 
@@ -22,30 +22,35 @@ def post_new(request):
         'form':form,
     })
 
-class DetailView(object):
-    '이전 FBV를 CBV버전으로 컨셉만 간단히 구현. 같은 동작을 수행'
-    def __init__(self, model):
-        self.model = model
 
-    def get_object(self, *args, **kwargs):
-        return get_object_or_404(self.model, id=kwargs['id'])
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
-    def get_template_name(self):
-        return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
 
-    def dispatch(self, request, *args, **kwargs):
-        return render(request, self.get_template_name(), {
-            self.model._meta.model_name: self.get_object(*args, **kwargs),
-        })
 
-    @classmethod
-    def as_view(cls, model):
-        def view(request, *args, **kwargs):
-            self = cls(model)
-            return self.dispatch(request, *args, **kwargs)
-        return view
-
-post_detail = DetailView.as_view(Post)
+# class DetailView(object):
+#     '이전 FBV를 CBV버전으로 컨셉만 간단히 구현. 같은 동작을 수행'
+#     def __init__(self, model):
+#         self.model = model
+#
+#     def get_object(self, *args, **kwargs):
+#         return get_object_or_404(self.model, id=kwargs['id'])
+#
+#     def get_template_name(self):
+#         return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         return render(request, self.get_template_name(), {
+#             self.model._meta.model_name: self.get_object(*args, **kwargs),
+#         })
+#
+#     @classmethod
+#     def as_view(cls, model):
+#         def view(request, *args, **kwargs):
+#             self = cls(model)
+#             return self.dispatch(request, *args, **kwargs)
+#         return view
+#
+# post_detail = DetailView.as_view(Post)
 
 # def generate_view_fn(model):
 #     def view_fn(request, id):
